8f1ce24e97e89afbe9d79040465eb7b8da3ee30d
ahale <andy@wwwdata.eu>
2013-04-18 21:38:10 +0200

    Update unmountcount.py

---
+++ unmountcount.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from swift.common.constraints import check_mount
+from swift.common.utils import whataremyips
+from swift.common.ring import Ring
+
+ring = Ring('/etc/swift/object.ring.gz')
+my_ips = whataremyips()
+mounted = 0
+drivecount = 0
+
+for dev in ring.devs:
+    try:
+        if dev['ip'] in my_ips and float(dev['weight']) > 0:
+            drivecount += 1
+            if check_mount('/srv/node', dev['device']):
+                mounted += 1
+    except TypeError:
+        pass
+
+unmounted = drivecount - mounted
+if unmounted > 0:
+    print unmounted
